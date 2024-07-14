9e0e06f03ac01ebc71fcda9b168568726c9cd11f
TranTruong245 <trancongtruongk1221@gmail.com>
2024-07-14 17:55:36 +0700

    config firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,17 +2,24 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: 'AIzaSyC6BDR8vZuUHiqt7VQkhLJ3pxYroNNjntA',
-    authDomain: 'ecom-chat-1d35c.firebaseapp.com',
-    projectId: 'ecom-chat-1d35c',
-    storageBucket: 'ecom-chat-1d35c.appspot.com',
-    messagingSenderId: '517909678281',
-    appId: '1:517909678281:web:ee31dcdc6180e6a3cee518',
-    measurementId: 'G-3X0P8VH3KR',
+    apiKey: 'AIzaSyBwVq5iMYCDsPgkK7t99faWnky40ZhUBNY',
+    authDomain: 'ecommerce-62a51.firebaseapp.com',
+    projectId: 'ecommerce-62a51',
+    storageBucket: 'ecommerce-62a51.appspot.com',
+    messagingSenderId: '1046064237505',
+    appId: '1:1046064237505:web:507952f5c7584a62ebfd40',
+    measurementId: 'G-3X8Q71JVVL',
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
 export default firebase;
 export const authentication = getAuth(initializeApp(firebaseConfig));
